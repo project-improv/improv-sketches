dfd84dc5c50d4c5a6e237474a02249ab40521e1f
chaichontat <34997334+chaichontat@users.noreply.github.com>
2019-09-25 15:32:18 -0400

    simGLM with MSE and LL plot

--- simGLM_flux.jl
+++ simGLM_flux.jl
@@ -1,5 +1,3 @@
-__precompile__()
-
 module simGLM
     using StaticArrays
     using LinearAlgebra: dot
@@ -18,8 +16,8 @@ module simGLM
         n::Int  # Iterations
     end
 
-    function PillowGLM(w::Array, h::Array, b::Array, data::Array, params::Dict)
-        M = size(data)[2]
+    function PillowGLM(w::Array, h::Array, b::Array, params::Dict)
+        M = params["numSamples"]
         N = params["numNeurons"]
         dh = params["hist_dim"]
         dt = params["dt"]
@@ -28,71 +26,76 @@ module simGLM
     end
 
     function ll(o::PillowGLM, data)
-        expo = ones(eltype(o.θ_h), o.N, o.M)
-        for j in 1:o.M
-            expo[:,j] = runModelStep(o, data[:, 1:j])
+        expo = ones(Tracker.TrackedReal{Float64}, o.N, o.M)
+
+        Threads.@threads for j in 1:o.M-o.dh
+            expo[:,j] = runModelStep(o, data[:, j:j+o.dh])
         end
 
         r̂ = o.dt.*exp.(expo)
 
-        return (sum(r̂) + sum(data .* log.(r̂)))  # Log-likelihood
+        (sum(r̂) + sum(data .* log.(r̂))) / o.M # Log-likelihood
     end
 
     function runModelStep(o::PillowGLM, data)
         t = size(data)[2] # data length in time
+        @assert t == 3
 
-        output = zeros(eltype(o.θ_h), o.N)
-        for i in 1:o.N  # Neuron
-            if t < 1
-                hist = 0
-            elseif t < o.dh
-                hist = sum(o.θ_h[i, 1:t] .* data[i, 1: t])
-            else
-                hist = sum(o.θ_h[i, :] .* data[i, t-o.dh+1: t])
-            end
-
-            if t < 2
-                weights = 0
-            else
-                weights = dot(o.θ_w[i,:], data[:, t-1])
-            end
-            output[i] = o.θ_b[i] + hist + weights
-        end
-
-        return output
+        [o.θ_b[i] + dot(o.θ_h[i, :], data[i, 2:end]) + dot(o.θ_w[i, :], data[:, end-1]) for i in 1:o.N]
+        # for i in 1:o.N  # Neuron
+        #     hist = dot(o.θ_h[i, :], data[i, 2:t])
+        #     weights = dot(o.θ_w[i,:], data[:, t-1])
+        #     output[i] = o.θ_b[i] + hist + weights
+        # end
     end
 
-    function fit!(o::PillowGLM, data; opt=Descent(0.1))
+    function fit!(o::PillowGLM, data; opt=Descent(1e-3))#, rate=x -> 1/x)
         o.n += 1
         grads = Tracker.gradient(() -> ll(o, data), Params([o.θ_b, o.θ_h, o.θ_w]))
         for p in (o.θ_b, o.θ_h, o.θ_w)
             update!(opt, p, grads[p])
         end
     end
-
 end
 
+using DelimitedFiles
 using Flux
 using Random
+using Plots
 
+cd("C:\\Users\\Chaichontat\\Documents\\GitHub\\improv-sketches")
 Random.seed!(82)
+data = parse.(Float64, readdlm("data_sample.txt", ' ', String, '\n'));
 
-N = 3
-M = 20
-dh = 3
+N = size(data)[1]
+M = 100
+dh = 2
 p = Dict("numSamples" => M, "numNeurons" => N, "hist_dim" => dh, "dt" => 0.1)
 
-weights = rand(N, N)
-hist = rand(N, dh)
-base = rand(N)
-data = randn(N, M) ./ 10
+weights = zeros(N, N)
+hist = zeros(N, dh)
+base = zeros(N)
 
-model = simGLM.PillowGLM(weights, hist, base, data, p)
+model = simGLM.PillowGLM(weights, hist, base, p)
 
-println(simGLM.ll(model, data))
-out = zeros(100)
-for i in 1:100
-    out[i] = simGLM.ll(model, data).data
-    simGLM.fit!(model, data, opt = ADAM(0.01))
+iter = 900
+
+out = zeros(iter)
+mean_sq = zeros(iter, 3)
+
+for i in 1:iter
+    out[i] = simGLM.ll(model, data[:, mod(i,300)+1:mod(i,300)+99+1]).data
+    if i > 1
+        println(i, out[i])
+    end
+    mean_sq[i, 1] = Flux.mse(model.θ_b.data, d["b"])
+    mean_sq[i, 2] = Flux.mse(model.θ_h.data, d["h"])
+    mean_sq[i, 3] = Flux.mse(model.θ_w.data, d["w"])
+
+    simGLM.fit!(model, data[:, mod(i,300)+1:mod(i,300)+99+1])
 end
-println(simGLM.ll(model, data))
+# plot(mean_sq[:, 1])
+# plot!(mean_sq[:, 2])
+plot(mean_sq[:, 1])
+plot!(mean_sq[:, 2])
+# plot(out)
