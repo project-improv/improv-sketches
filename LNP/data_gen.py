15ebc74c948fd9ed78f45e86911cb85446859d5a
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-03-15 15:41:48 -0400

    Add multithreaded data generation.

--- LNP/data_gen.py
+++ LNP/data_gen.py
@@ -1,9 +1,12 @@
 import pickle
 
-import numpy as np
-from scipy.stats import poisson
+from multiprocessing import Pool
+
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+
+from scipy.stats import poisson
 
 sns.set()
 
@@ -31,11 +34,10 @@ def generateModel(params):
     theta['w']: coupling filters for the n neurons (N x N) aka weight matrix
     theta['b']: baseline firing (N,)
     '''
-    np.random.seed(42)
+    np.random.seed(2)
     # get parameters
     dh = params['hist_dim']
     N = int(params['numNeurons'])
-    M = int(params['numSamples'])
     alpha = params['alpha']
 
     # store model as dictionary
@@ -45,13 +47,13 @@ def generateModel(params):
     fracInh = 0.2
     numInh = int(np.ceil(fracInh * N))
     numExc = N - numInh
-    inh = -0.05 * np.random.rand(N, numInh)
-    exc = 0.5 * np.random.rand(N, numExc)
+    inh = -0.6 * np.random.rand(N, numInh)
+    exc = 11 * np.random.rand(N, numExc)
 
     # baseline rates
     theta['b'] = np.zeros((N))
-    theta['b'][:numInh] = 0.1 * np.ones((1, numInh))
-    theta['b'][numInh:] = -0.8 + np.random.rand(1, numExc)
+    theta['b'][:numInh] = 3.5 * np.ones((1, numInh))
+    theta['b'][numInh:] = 1.5 + np.random.rand(1, numExc)
 
     # sparsity
     temp = np.random.rand(N, numExc)
@@ -71,13 +73,13 @@ def generateModel(params):
     tau = np.linspace(1, 0, dh).reshape((dh, 1))
     tauInh = tau.dot(np.ones((1, numInh)))
     tauExc = tau.dot(np.ones((1, numExc)))
-    theta['h'][:numInh, :] = -0.02 * np.exp(-10 * tauInh).T  # mag (1.5e-3, 2e-1)
-    theta['h'][numInh:, :] = 0.05 * np.exp(-5 * tauExc).T  # mag (7e-2, 5e-1)
+    theta['h'][:numInh, :] = -0.05 * np.exp(-4 * tauInh).T / p['dt']  # mag (1.5e-3, 2e-1)
+    theta['h'][numInh:, :] = -0.05 * np.exp(-3 * tauExc).T / p['dt']  # mag (7e-2, 5e-1)
 
     return theta
 
 
-def generateData(theta, params):
+def generateData(theta, params, m):
     '''
     Generates spike counts from the model
     Returns a data dict:
@@ -87,7 +89,7 @@ def generateData(theta, params):
     # constants
     dh = params['hist_dim']
     N = int(params['numNeurons'])
-    M = int(params['numSamples'])
+    # M = int(params['numSamples'])
 
     # nonlinearity; exp()
     f = params['f']
@@ -98,16 +100,16 @@ def generateData(theta, params):
     b = theta['b']
 
     # store output in a dictionary
-    data = {'y': np.zeros((N, M)), 'r': np.zeros((N, M))}  # spikes, rates
+    data = {'y': np.zeros((N, m)), 'r': np.zeros((N, m))}  # spikes, rates
 
     # the initial rate (no history); generate randomly
-    init = 0.1 * np.random.randn(M)
+    init = 2 * np.random.randn(m)
     data['y'][:, 0] = poisson.rvs(f(init[0]))
     data['r'][:, 0] = f(init[0])
 
     # simulate the model for next M samples (time steps)
-    for j in np.arange(0, M):  # step through time
-        for i in np.arange(0, N):  # step through neurons
+    for j in range(m):  # step through time
+        for i in range(N):  # step through neurons
             # compute model firing rate
             if j < 1:
                 hist = 0
@@ -125,17 +127,20 @@ def generateData(theta, params):
 
             # draw spikes
             data['r'][i, j] = r
-            data['y'][i, j] = poisson.rvs(r)
+            data['y'][i, j] = poisson.rvs(r * p['dt'])
 
     return data
 
 
 if __name__ == '__main__':
-    p = setParameters(n=20, dh=2, m=1e5, alpha=0.08)
+    p = setParameters(n=50, dh=10, m=1e5, alpha=0.05)
 
     print('Generating model...')
     theta = generateModel(p)
-    plt.imshow(np.abs(theta['w']))
+    scale = np.max(np.abs(theta['w']))
+    x = plt.imshow(theta['w'], vmin=-scale, vmax=scale)
+    x.set_cmap('bwr')
+    plt.grid(False)
     plt.colorbar()
     plt.show()
 
@@ -145,7 +150,20 @@ if __name__ == '__main__':
         pickle.dump(p, f)
     print(theta['w'])
     print('Simulating model...')
-    data = generateData(theta, p)
+
+    cores = 4
+    M = p['numSamples']
+    M_core = [M // cores] * (cores - 1) + [M - M // cores * (cores - 1)]
+
+    def proc(m):
+        return generateData(theta, p, m)
+
+    with Pool(cores) as pool:
+        futures = list(pool.map(proc, M_core))
+
+    y, r = np.hstack([d['y'] for d in futures]), np.hstack([d['r'] for d in futures])
+    data = {'y': y, 'r': r}
+
     print('Spike Counts:')
     print('mean: ', np.mean(data['y']))
     print('var.: ', np.var(data['y']), '\n')
@@ -153,6 +171,7 @@ if __name__ == '__main__':
     print('mean: ', np.mean(data['r']))
     print('var.: ', np.var(data['r']))
     print('*** %g percent of rates are over the limit. ***' % (100 * np.mean(data['r'] > 10)))
+    print(np.percentile(data['y'], [50, 90, 99]))
 
     np.savetxt('data_sample.txt', data['y'])
     np.savetxt('rates_sample.txt', data['r'])
